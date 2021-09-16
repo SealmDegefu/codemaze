588318656f26af838f9524f05462b72d169f5cac
Andrew McAllister <andymac7@sbcglobal.net>
2021-09-16 13:34:11 -0500

    Finished the game board

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -19,7 +19,11 @@ function Game() {
 		<h1>I am a trophy</h1>
 			
 		<div className="container">
-    		<div className="instructions"> Content A</div>
+    		<div className="instructions">
+				<h2>Instructions</h2>
+				Text about the game!
+			
+			</div>
 	
     		<div className="maze">
 		<Box sx={{ flexGrow: 1 }}>
@@ -29,21 +33,111 @@ function Game() {
           	{/* <Item> </Item> */}
         	</Grid>
         	<Grid item xs={1}>
-          	<Item className="grid-size"> <MdAccessibility size={70}/></Item>
+          	<Item className="grid-size" id="start">Start<MdAccessibility size={70}/></Item>
         	</Grid>
-				{/* Row 2 */}
+			{/* Row 2 */}
 			<Grid className="maze-background" item xs={3}>
           	{/* <Item> </Item> */}
         	</Grid>
 			<Grid item xs={1}>
-          	<Item className="grid-size"></Item>
+          	<Item className="grid-size" id="3">First Challenge</Item>
         	</Grid>
 			<Grid item xs={1}>
-          	<Item className="grid-size"></Item>
+          	<Item className="grid-size" id="2"></Item>
         	</Grid>
 			<Grid item xs={1}>
-          	<Item className="grid-size"></Item>
+          	<Item className="grid-size" id="1"></Item>
         	</Grid>
+			{/* Row 3 */}
+			<Grid className="maze-background" item xs={1}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="8">Optional</Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={1}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="4"></Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={2}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			{/* Row 4 */}
+			<Grid className="maze-background" item xs={1}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="7"></Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="6">Second Challenge</Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="5"></Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={2}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			{/* Row 5 */}
+			<Grid className="maze-background" item xs={2}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="9"></Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={3}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			{/* Row 6 */}
+			<Grid className="maze-background" item xs={2}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="10">Third Challenge</Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="11"></Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="12"></Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="13">Optional</Item>
+        	</Grid>
+			{/* Row 7 */}
+			<Grid className="maze-background" item xs={2}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="14"></Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={3}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			{/* Row 8 */}
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="17"></Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="16"></Item>
+        	</Grid>
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="15">Last Challenge</Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={3}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+			{/* Row 9 */}
+			<Grid item xs={1}>
+          	<Item className="grid-size" id="end">End</Item>
+        	</Grid>
+			<Grid className="maze-background" item xs={5}>
+          	{/* <Item> </Item> */}
+        	</Grid>
+
+
       		</Grid>
     	</Box>
 		
